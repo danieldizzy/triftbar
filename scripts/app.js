2d6c1491350fc18d5aae77ac825a2e1758e12896
Daniel Dizzy <dizzylol4@gmail.com>
2015-04-11 00:44:49 +0900

    edited the script

--- scripts/app.js
+++ scripts/app.js
@@ -9,15 +9,19 @@ var app = angular
   ])
   .constant('FURL', 'https://triftbar.firebaseio.com//')  
   .config(function ($routeProvider) {
-    $routeProvider      
+    $routeProvider    
       .when('/', {
+        templateUrl: 'views/main.html'        
+      })  
+      .when('/post', {
         templateUrl: 'views/browse.html'        
       })
-      .when('/post', {
-        templateUrl: 'views/post.html'        
-      })
+
       .when('/edit', {
         templateUrl: 'views/edit.html'        
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html'        
       })
       .otherwise({
         redirectTo: '/'
