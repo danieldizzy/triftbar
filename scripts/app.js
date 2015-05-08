b5306bdb22d01638b3896476029cdaf6f51c068a
Daniel Dizzy <dizzylol4@gmail.com>
2015-05-09 02:36:44 +0900

    created the nav bar

--- scripts/app.js
+++ scripts/app.js
@@ -7,21 +7,32 @@ var app = angular
     'ngRoute',    
     'firebase'
   ])
-  .constant('FURL', 'https://triftbar.firebaseio.com//')  
+  .constant('FURL', 'https://triftbar.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider    
       .when('/', {
         templateUrl: 'views/main.html'        
       })  
+      .when('/login', {
+        templateUrl: 'views/login.html', 
+        controller: 'ItemController'        
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html', 
+        controller: 'ItemController'        
+      })
       .when('/post', {
-        templateUrl: 'views/post.html'        
+        templateUrl: 'views/post.html', 
+        controller: 'ItemController'        
       })
 
-      .when('/edit', {
-        templateUrl: 'views/edit.html'        
+      .when('/edit/:itemsId', {
+        templateUrl: 'views/edit.html',
+        controller: 'ItemController'        
       })
       .when('/browse', {
-        templateUrl: 'views/browse.html'        
+        templateUrl: 'views/browse.html',
+        controller: 'ItemController'
       })
       .otherwise({
         redirectTo: '/'
